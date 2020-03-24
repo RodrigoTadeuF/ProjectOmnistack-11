0e91f9c6e1ef42626fa9f4049bd6f9ce710910cd
Rodrigo Tadeu <rodrigotfonsecas@outlook.com>
2020-03-24 12:10:03 -0300

    adding backend done and frontend folder

---
+++ backend/src/index.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const cors = require('cors')
+const routes = require('./routes');
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(routes); 
+
+app.listen(3333);
+
+/*
+ *Rotas / Recurso 
+ */
+
+/**
+ * Métodos HTTP
+ * 
+ * GET: Usado quando queremos BUSCAR uma informação do back-end
+ * POST: Usado quando queremos CRIAR uma informação no back-end
+ * PUT: Usando quando queremos ALTERAR uma informação no back-end
+ * DELETE: Usado quando queremos DELETAR uma informação no back-end
+ */
+
+/**
+ * Tipos de Parâmetros:
+ * 
+ * Query Params: Parâmetros nomeados enviados na rota após o símbolo de "?"  (Filtros, paginação)
+ * Route Params: Parâmetros utilizados para identificar recursos
+ * Request Body: Corpo da requisição, utilizado para criar ou alterar recursos
+*/
+
+/**
+ * SQL: MySQL, SQLite, PostgreSQL, Oracle, Microsoft SQL Server
+ * NoSQL: MongoDB, Neo4j, Redis, Cassandra 
+ */
+
+ /**
+  * Driver: SELECT * FROM users
+  * Query Builder: table('users')select('*').where()
+  */
